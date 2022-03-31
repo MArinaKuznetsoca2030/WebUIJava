158cd7f76253582e72dc174ed937d47fba2cca96
MKuznetsova <param2030@mail.ru>
2022-03-31 19:43:31 +0600

    add lesson4

---
+++ src/main/java/lesson4/AreaOfTheTriangle.java
@@ -0,0 +1,13 @@
+package lesson4;
+
+import static java.lang.Math.sqrt;
+
+public class AreaOfTheTriangle {
+
+    public static float areaTringle(float a, float b, float c){
+        float p = (a + b + c)/2;
+        return (float) sqrt((p * (p - a) * (p - b) *(p - c)));
+
+
+    }
+}
